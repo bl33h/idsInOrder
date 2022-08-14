80fa8bc8e511bf75f55bea9bade7c253e1ccc28e
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-08-13 19:24:21 -0600

    final version of code.

--- IDsInOrder.cpp
+++
@@ -1,8 +0,0 @@
-/*---------------------------------------------------------------------------
-Copyright (C), 2022-2023, Sara Echeverria (bl33h)
-@author Sara Echeverria
-FileName: IDsInOrder
-@version: I
-Creation: 11/08/2022
-Last modification: 11/08/2022
-------------------------------------------------------------------------------*/
---
+++ src/IDsInOrder.cpp
@@ -0,0 +1,45 @@
+/*---------------------------------------------------------------------------
+Copyright (C), 2022-2023, Sara Echeverria (bl33h)
+@author Sara Echeverria
+FileName: IDsInOrder
+@version: I
+Creation: 11/08/2022
+Last modification: 13/08/2022
+------------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+// Main method
+int main(){
+    requestIDnumber();
+    IDsInOrder();
+    delete[] ID;
+
+    return 0;
+}
+
+// ID request method
+    void requestIDnumber(){
+    cout<<"Enter the number of IDs to order: "; // Number of spaces the array will have
+    cin>>IDnumber;
+    ID new int[IDnumber]; // Creation of the array
+    for (int i = 0; i < IDnumber;i++){
+        cout <<"Name: ";
+        cout <<"Lastname: ";
+        cout <<"Last number of the CUI: ";
+        cout << "Enter an ID: ";
+        cin >> ID[i];
+    }
+}
+
+// IDs in order method
+    void IDsInOrder(){ 
+    sort(ID, ID+IDnumber);
+    cout<<"Enter the number of IDs you placed in the input: "; // Number of spaces the array has
+    cin>>IDnumber;
+    for (int i = 0; i < IDnumber; ++i) 
+    cout << ID[i] << " ";   
+    return 0; 
+} 
